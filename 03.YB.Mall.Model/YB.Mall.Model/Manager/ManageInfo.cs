317a93fdf2cd65c92a9fb455b9433628c4457c22
KozoWo1aspx <kozo.wo1aspx@gmail.com>
2016-10-12 10:19:27 +0800

    model

---
+++ 03.YB.Mall.Model/YB.Mall.Model/Manager/ManageInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace YB.Mall.Model
+{
+    public class ManageInfo
+    {
+        public int ManageId { get; set; }
+        public string ManageName { get; set; }
+        public int? Status { get; set; }
+        public int? RoleId { get; set; }
+        public string Phone { get; set; }
+    }
+}
