b738ba2e1e8e19d319e1f57791871457534a4965
KozoWo1aspx <kozo.wo1aspx@gmail.com>
2016-10-12 13:00:29 +0800

    content

---
+++ 02.YB.Mall.Data/YB.Mall.Data/Repositories/Repository/ManageRepository.cs
@@ -0,0 +1,14 @@
+﻿using YB.Mall.Data.Infrastructure;
+using YB.Mall.Model;
+
+namespace YB.Mall.Data.Repositories
+{
+    public class ManageRepository:Repository<ManageInfo>,IManageRepository
+    {
+        public ManageRepository(IDatabaseFactory dbFactory)
+            : base(dbFactory)
+        {
+            
+        }
+    }
+}
